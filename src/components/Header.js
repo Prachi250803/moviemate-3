374a4ec0e9e9f300b088868d777c125aedaad2a2
Prachi250803 <72382688+Prachi250803@users.noreply.github.com>
2024-06-13 22:00:04 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,13 +4,15 @@ import { Link, NavLink, useNavigate } from "react-router-dom";
 import Logo from "../assets/moviematelogo.jpg";
 
 export const Header = ({ onSearch }) => {
+  const navigate = useNavigate();
   const handleSearch = (event) => {
     const term = event.target.value;
     onSearch(term);
+    navigate(`/search?q=${term}`);
   }
   const [hidden, setHidden] = useState(true);
   const [darkMode, setDarkMode] = useState( JSON.parse(localStorage.getItem("darkMode")) || false);
-  const navigate = useNavigate();
+  
 
   useEffect(() => {
     localStorage.setItem("darkMode", JSON.stringify(darkMode));
@@ -28,9 +30,9 @@ export const Header = ({ onSearch }) => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    const queryTerm = event.target.search.value;
+    const queryterm = event.target.search.value;
     event.target.reset();
-    return navigate(`/search?q=${queryTerm}`);
+    return navigate(`/search?q=${queryterm}`);
   }
 
   return (
@@ -55,8 +57,8 @@ export const Header = ({ onSearch }) => {
                 <svg className="w-5 h-5 text-gray-500" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd"></path></svg>
                 <span className="sr-only">Search icon</span>
               </div>
-              <form onSubmit={handleSubmit}>
-                <input onChange={handleSearch} type="text" id="search-navbar" name="search" className="block p-2 pl-10 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:text-sm focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search..." autoComplete="off" />
+              <form onSubmit={handleSubmit} onChange={handleSearch}>
+                <input type="text" id="search-navbar" name="search" className="block p-2 pl-10 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:text-sm focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search..." autoComplete="off" />
               </form>
             </div>
             <button onClick={() => setHidden(!hidden)} data-collapse-toggle="navbar-search" type="button" className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-search" aria-expanded="false">
@@ -70,7 +72,7 @@ export const Header = ({ onSearch }) => {
                 <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
                   <svg className="w-5 h-5 text-gray-500" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd"></path></svg>
                 </div>
-                <form onSubmit={handleSubmit}>
+                <form onSubmit={handleSubmit} onChange={handleSearch}>
                   <input type="text" id="search-navbar" name="search" className="block p-2 pl-10 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:text-sm focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search..." autoComplete="off" />
                 </form>
               </div>
